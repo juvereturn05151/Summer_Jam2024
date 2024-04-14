52d2bd7679a26ca45f1c7fa86f9a6e566943410a
juvereturn@gmail.com <Rusty05151>
2024-04-15 04:33:13 +0700

    Comment human manager

--- Assets/Script/Sunlight.cs
+++ Assets/Script/Sunlight.cs
@@ -73,7 +73,7 @@ public class Sunlight : MonoBehaviour
                 if (_currentAlpha >= _maximumAlpha || light.intensity >= sunlightFlashIntensity)
                 {
                     ActivateSunlight = true;
-                    HumanManager.Instance.MoveHere(this.transform.position);
+                    //HumanManager.Instance.MoveHere(this.transform.position);
                 }
             }
             else
