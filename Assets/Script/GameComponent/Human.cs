9d091c916660b3d3abc7c1270cbff8c3de1aea5e
qiqitaro <thanajan.saes@bumail.net>
2024-04-15 14:08:43 +0700

    push

--- Assets/Script/GameComponent/Human.cs
+++ Assets/Script/GameComponent/Human.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 
 public class Human : MonoBehaviour
 {
-    [SerializeField]
-    private HumanAIAgent _humanAIAgent;
+    [SerializeField] private HumanAIAgent _humanAIAgent;
     public HumanAIAgent HumanAIAgent => _humanAIAgent;
 
     [SerializeField] private float health;
