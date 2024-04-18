9d4d8a8a38920c9cd77e62a95118c476eb4c9cd6
juvereturn@gmail.com <Rusty05151>
2024-04-18 19:27:04 +0700

    polish tutorial 1

--- Assets/Script/Managers/TutorialUIManager.cs
+++ Assets/Script/Managers/TutorialUIManager.cs
@@ -5,6 +5,65 @@ using UnityEngine.SceneManagement;
 
 public class TutorialUIManager : MonoBehaviour
 {
+    public GameObject[] tutorialPages; // Array of tutorial pages
+    private int currentPageIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowPage(currentPageIndex); // Show the first page when the tutorial starts
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // You can also implement keyboard shortcuts or buttons for navigation
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ShowPreviousPage();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            ShowNextPage();
+        }
+    }
+
+    // Show the specified page and hide others
+    void ShowPage(int pageIndex)
+    {
+        for (int i = 0; i < tutorialPages.Length; i++)
+        {
+            if (i == pageIndex)
+            {
+                tutorialPages[i].SetActive(true);
+            }
+            else
+            {
+                tutorialPages[i].SetActive(false);
+            }
+        }
+    }
+
+    // Show the previous page
+    public void ShowPreviousPage()
+    {
+        if (currentPageIndex > 0)
+        {
+            currentPageIndex--;
+            ShowPage(currentPageIndex);
+        }
+    }
+
+    // Show the next page
+    public void ShowNextPage()
+    {
+        if (currentPageIndex < tutorialPages.Length - 1)
+        {
+            currentPageIndex++;
+            ShowPage(currentPageIndex);
+        }
+    }
+
     public void Play()
     {
         SceneManager.LoadScene("QiqiRealGameplay");
